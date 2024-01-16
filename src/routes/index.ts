91a2f9749fd9fb979e331ba74fa18a4107ba9afb
ehdtjs0612 <inko51366@naver.com>
2024-01-16 18:30:00 +0900

    feat: account 라우트 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import testRouter from './test';
 import postsRouter from './post';
+import accountRouter from './account';
 
 const router = Router();
 
 router.use('/test', testRouter);
 router.use('/posts', postsRouter);
+router.use('/account', accountRouter);
 
 export default router;
